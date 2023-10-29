068f3316507ed07517c3deb949e1a220d9a75880
Jintu James <jintujames444@gmail.com>
2023-10-29 11:54:42 +0530

    atles connected

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 
 const app = express();
- 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/Lancome');
+
+// MongoDB connection URI
+const url = 'mongodb+srv://jintujames444:refreshing@cluster0.bhbg5kr.mongodb.net/Essence?retryWrites=true&w=majority';
+
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+           // You can remove this line, as it's not necessary in recent versions of Mongoose.
+            tlsAllowInvalidCertificates: true,  // Use this for testing with self-signed certificates.
+        });
+
+        console.log('db connected');
+    } catch (error) {
+        console.log('mongo db connection error', error);
+    }
+}
+
+dbConnect();
+
+
+
 
 const userRoute = require('./routes/userRoute');
 const adminRoute = require('./routes/adminRoute');
