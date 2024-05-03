ec68a60ca3a2405fa10c3791b0ad7d13ddbc6dfb
Jintu James <jintujames444@gmail.com>
2024-05-03 16:10:45 +0530

    render

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://127.0.0.1:27017/Lancome');
 
 
-const url='mongodb+srv://jintujames444:refreshing@cluster0.bhbg5kr.mongodb.net/Essenc?retryWrites=true&w=majority'
+const url='mongodb+srv://jintujames444:refreshing@cluster0.bhbg5kr.mongodb.net/Essence?retryWrites=true&w=majority'
 const dbConnect=async(req,res)=>{
     try {
         await mongoose.connect(url, {
@@ -36,4 +36,6 @@ app.use('/admin',adminRoute);
 
 app.listen(2001, () => {
     console.log('server started');
+
+    
 })
